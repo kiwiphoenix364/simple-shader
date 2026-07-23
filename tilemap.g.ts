1bec54ca7da17f0b2340c8b04e99d42bc33663ba
kiwiphoenix <kiwiphoenix364@gmail.com>
2026-07-23 15:53:44 -0400

    Refactor transparency16 export to remove try block

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -1,10 +1,7 @@
 // Auto-generated code. Do not edit.
 namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
-    try {
-        export const transparency16 = image.ofBuffer(hex``);
-    } finally {
-    }
+    export const transparency16 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
